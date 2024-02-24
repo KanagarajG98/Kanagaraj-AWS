14f4fa3d4fbc2d33ef17879619bfc7485ed9f5f5
KanagarajG98 <159407857+KanagarajG98@users.noreply.github.com>
2024-02-24 22:30:54 +0530

    1

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull kanagarajgk/sample-aws-cicd-app
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 abhishekf5/simple-python-flask-app
+docker run -d -p 8000:8000 kanagarajgk/sample-aws-cicd-app
