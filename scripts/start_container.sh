219cc1c78a91a10191318369684bff9d1fe930b1
KanagarajG98 <159407857+KanagarajG98@users.noreply.github.com>
2024-02-24 17:14:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull kanagarajgk/sample-aws-cicd-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 kanagarajgk/sample-aws-cicd-app
+docker run -d -p 5000:5000 kanagarajgk/sample-aws-cicd-app 
