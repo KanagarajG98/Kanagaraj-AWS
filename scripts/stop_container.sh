92cd7aaefe2cfed409ae82bd0f54af3c805a1519
KanagarajG98 <159407857+KanagarajG98@users.noreply.github.com>
2024-02-25 16:42:28 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop the running container
-docker stop my_container
-
-# Remove the stopped container
-docker rm my_container
-
-# Exit with a success status code
-exit 0
+# Stop the running container (if any)
+echo "Hi"
