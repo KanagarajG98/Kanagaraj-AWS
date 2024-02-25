9cf71d63316b7a8faf9f3667adddcf4147f8c2a5
KanagarajG98 <159407857+KanagarajG98@users.noreply.github.com>
2024-02-25 17:11:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Set executable permissions on start_container.sh
+chmod +x /opt/codedeploy-agent/deployment-root/56943513-bed2-4339-82fa-df0826b38fee/d-PG1OE17E3/deployment-archive/scripts/start_container.sh
+
 # Pull the Docker image from Docker Hub
 docker pull kanagarajgk/sample-aws-cicd-app
 
